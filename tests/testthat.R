f695f06a945cf7538afb5afcfff41fcc8dc9d77f
Beniamino Green <ben@greendalba.com>
2022-10-25 12:03:28 -0400

    changed package name in testing code

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(exhaustivetree)
+library(parallelpolicytree)
 
-test_check("exhaustivetree")
+test_check("parallelpolicytree")
